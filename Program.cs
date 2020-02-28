9d6bbf848123379cd2b9492454c7aa2960a2a511
ZAKARY SMITH <6820492@student.swin.edu.au>
2020-02-28 17:05:48 +1100

    First Exercise Complete

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your first name: ");
+            string first = Console.ReadLine();
+            Console.WriteLine("Please enter your surname: ");
+            string second = Console.ReadLine();
+            Console.WriteLine("Enter your student ID: ");
+            string ID = Console.ReadLine();
+
+
+            Console.WriteLine("Enter year of birth: ");
+            int birth =  int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the current year: ");
+            int year = int.Parse(Console.ReadLine());
+            int age = year - birth;
+
+
+            Console.WriteLine("Hi, " + first + " " + second + ". You are " + age + "!");
+            Console.ReadLine();
+        }
+    }
+}
