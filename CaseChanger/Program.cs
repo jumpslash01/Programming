f44ee80aa20e646fe241ea494f82270d5cb3754c
Zak <6820492@student.swin.edu.au>
2020-03-05 17:55:28 +1100

    files sorted

---
+++ CaseChanger/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Programming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            string text = "";
+
+
+            while (text != "quit")
+            {
+                Console.WriteLine("Enter some text: ");
+                text = Console.ReadLine();
+                int length = text.Length;
+                while (a < length)
+                {
+                    if (Char.IsLower(text[a]))
+                    {
+                        Console.Write(char.ToUpper(text[a]));
+                        a++;
+                    }
+                    else
+                    {
+                        Console.Write(char.ToLower(text[a]));
+                        a++;
+                    }
+                }
+                a = 0;
+                Console.WriteLine();
+            }
+        }
+    }
+}
