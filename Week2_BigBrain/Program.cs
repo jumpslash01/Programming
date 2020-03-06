8a25cebe41ca4960376d3bf12a61965701de2d67
ZAKARY SMITH <6820492@student.swin.edu.au>
2020-03-06 16:49:29 +1100

    Teaches Idea

--- Week2_BigBrain/Program.cs
+++ Week2_BigBrain/Program.cs
@@ -1,8 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Week2_BigBrain {
     class Program {
         static void Main (string[] args) {
+
+            List<User> users = new List<User>();
+
+            users.Add(new User("tim", "password1"));
+            users.Add(new User("bob", "password2"));
+            users.Add(new User("fred", "password3"));
+            users.Add(new User("bill", "password4"));
+  
+            Console.WriteLine("Username");
+            string userInput = Console.ReadLine();
+
+            Console.WriteLine("Password");
+            string passInput = Console.ReadLine();
+           
+           
+            User currentUser = users.Find(i => i.username == userInput);
+
+            if (currentUser == null || currentUser.password != passInput)
+            {
+                Console.WriteLine("Invalid Login");
+            }
+            else
+            {   
+                Console.WriteLine("Login Successful");
+            }
+
+
+            /*
             string[] users = { "Zak", "Liz", "Dan", "Phil", "Dirk", "Joe", "Sally" };
             string[] pass = { "1234", "2345", "3564", "4567", "3546", "4876", "6851" };
 
@@ -21,6 +50,7 @@ namespace Week2_BigBrain {
                 }
             }
             Console.WriteLine ("Fail");
+            */
         }
     }
 }
---
+++ Week2_BigBrain/User.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Week2_BigBrain {
+    class User {
+
+            public string username;
+            public string password;
+
+            public User(string pUsername, string pPassword)
+            {
+                username = pUsername;
+                password = pPassword;
+            }
+  
+    }
+}
