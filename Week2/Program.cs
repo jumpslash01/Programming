8f4edfb0577513c4aacb376ed4cac6540c129a6d
ZAKARY SMITH <6820492@student.swin.edu.au>
2020-03-06 14:47:00 +1100

    week2 addition

---
+++ Week2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Week2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter Username then Password");
+            string a = (Console.ReadLine());
+            string b = (Console.ReadLine());
+               
+            if (a == "test")
+            {
+                if (b == "password")
+                
+                    Console.WriteLine("login successful!");
+            }
+            else Console.WriteLine("Login Failed");
+
+        }
+    }
+}
