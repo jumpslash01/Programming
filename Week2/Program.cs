7c73a273bbb69913adb88afa0736b2e84c164904
ZAKARY SMITH <6820492@student.swin.edu.au>
2020-03-06 15:59:58 +1100

    added big brain

--- Week2/Program.cs
+++ Week2/Program.cs
@@ -1,23 +1,43 @@
 ﻿using System;
 
-namespace Week2
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Please enter Username then Password");
-            string a = (Console.ReadLine());
-            string b = (Console.ReadLine());
-               
-            if (a == "test")
-            {
-                if (b == "password")
-                
-                    Console.WriteLine("login successful!");
-            }
-            else Console.WriteLine("Login Failed");
+namespace Week2 {
+    class Program {
+        static void Main (string[] args) {
+            Console.WriteLine ("Please enter Username then Password");
+            string a = (Console.ReadLine ());
+            string b = (Console.ReadLine ());
 
+            if (a == "Zak") {
+                if (b == "1234")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            } else if (a == "Liz") {
+                if (b == "2345")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            } else if (a == "Dan") {
+
+                if (b == "3564")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            } else if (a == "Phil") {
+                if (b == "4567")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            } else if (a == "Dirk") {
+                if (b == "3546")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            } else if (a == "Joe") {
+                if (b == "4876")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            } else if (a == "Sally") {
+                if (b == "6851")
+                    Console.WriteLine ("login successful!");
+                else Console.WriteLine ("Login Failed");
+            }
         }
+
     }
 }
---
+++ Week2_BigBrain/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Week2_BigBrain {
+    class Program {
+        static void Main (string[] args) {
+            string[] users = { "Zak", "Liz", "Dan", "Phil", "Dirk", "Joe", "Sally" };
+            string[] pass = { "1234", "2345", "3564", "4567", "3546", "4876", "6851" };
+
+            string username = Console.ReadLine ();
+            string password = Console.ReadLine ();
+
+            foreach (string name in users) {
+                if (username == name) {
+                    int b = Array.IndexOf (users, username);
+                    Console.WriteLine(b);
+
+                    if (password == pass[b]) {
+                        Console.WriteLine ("There is a success");
+                        System.Environment.Exit (1);
+                    }
+                }
+            }
+            Console.WriteLine ("Fail");
+        }
+    }
+}
